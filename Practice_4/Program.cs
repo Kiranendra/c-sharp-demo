9c954ced6810e8b1d0037cf95baa32916792f023
Kiranendra <vivs8899@gmail.com>
2020-10-09 16:14:38 +0530

    Updated Practice_4 code a little more

--- Practice_4/Program.cs
+++ Practice_4/Program.cs
@@ -9,6 +9,14 @@ namespace Practice_4
             Console.WriteLine("Hello World!");
             SecondProgram secondProgram = new SecondProgram();
             Console.WriteLine(secondProgram.count);
+
+            // Here, the variable name is inaccessible
+            // Console.WriteLine(secondProgram.name); 
+            
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("Value of the 'name' variable before setting: " + secondProgram.GetName());
+            secondProgram.SetName("The name is set now");
+            Console.WriteLine("Value of the 'name' variable after setting: " + secondProgram.GetName());
         }
     }
 }
--- Practice_4/SecondProgram.cs
+++ Practice_4/SecondProgram.cs
@@ -4,5 +4,26 @@ namespace Practice_4
     {
         // Must be public to call in another class
         public int count = 9;
+        
+        // Instead of making the variable public it is good practice to 
+        // use methods to get and set the data.
+
+
+        // This variable is not accessible outside this class 
+        // (with or without 'private' keyword)
+        private string name = "No name is set";
+
+        // Must be public to access
+        public string GetName()
+        {
+            return name;
+        }
+
+        // Must be public to access
+        public void SetName(string name)
+        {
+            // 'this' keyword is used to refer the current object.
+            this.name = name;
+        }
     }
 }
